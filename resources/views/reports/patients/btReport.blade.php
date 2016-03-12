84199716b3d2fcbe1eddca2999cf160ed746845f
purnenduDev <purnendu.chandan@gmail.com>
2016-03-12 12:25:45 +0530

    remove pagination

--- resources/views/reports/patients/btReport.blade.php
+++ resources/views/reports/patients/btReport.blade.php
@@ -5,7 +5,7 @@
       <div class="pull-right" style="">
         <a name="download" id="downloadCSV" class="btn btn-primary" download="filename.csv">Download Csv</a>   
         <input type="hidden" name="csv_text" id="csv_text"> 
-        <button type="button" id="edit" class="btn btn-success">Edit</button>        
+       
       </div>
             <h4>Patient BT Report</h4>           
         </div>
@@ -42,84 +42,34 @@
 
                         @foreach ($patients as $patient)
                             <tr>
-                                <td><a href="/lead/{{$patient->lead_id}}/viewDetails" target="_blank">{{ $patient->lead->name or "No Name" }}</a>
-                                    <div class="pull-right">
-                                        
-                                       {{$patient->name}}
-                                    </div>
-                               </td>
-                               <td>
-                                {{ $patient->lead->gender or " " }} 
-                               </td>
-                               <td>
-                                    <?php
+                               <td><a href="/lead/{{$patient->lead_id}}/viewDetails" target="_blank">{{ $patient->lead->name or "No Name" }}</a><div class="pull-right">{{$patient->name}}</div></td>
+                               <td>{{ $patient->lead->gender or " " }}</td>
+                               <td><?php
                                     if(isset($patient->lead->dob))
-                                    echo date_diff(date_create($patient->lead->dob), date_create('today'))->y;
-                                    
-                                    ?>
-                               </td>
-                              <td>
-                              {{date('jS M, Y', strtotime($patient->entry_date))}}
-                              
-                              </td>
-                               <?php
-                              
-                                
-                                  $patient_prakriti = \App\Models\PatientPrakriti::prakriti($patient->id);
-                                
-                               
-                               ?><td>
-                                   {{$patient_prakriti->first_dominant_name}}
-                                   </td>
-                                   <td>
-                                   {{round($patient_prakriti->first_dominant_percentage, 2)}}%
-                               </td>
-                               <td>
-                                   {{$patient_prakriti->second_dominant_name}}
-                                   </td>
-                                   <td>
-                                   {{round($patient_prakriti->second_dominant_percentage, 2)}}%
-                               </td>
-                               <td>
-                                   {{$patient_prakriti->recessive_name}}
-                                   </td>
-                                   <td>
-                                   {{round($patient_prakriti->recessive_percentage, 2)}}%
-                               </td>
-                                    
-                                   
-                              
-                                
-                                <td>{{($patient->blood_type)?$patient->blood_type->name:''}}</td>
-                                <td>{{($patient->rh_factor)?$patient->rh_factor->code:''}}</td>
+                                    echo date_diff(date_create($patient->lead->dob), date_create('today'))->y; ?></td>
+                               <td>{{date('jS M, Y', strtotime($patient->entry_date))}}</td>
+                               <?php                             
+                                $patient_prakriti = \App\Models\PatientPrakriti::prakriti($patient->id);
+                                ?>
+                               <td>{{$patient_prakriti->first_dominant_name}}</td>
+                               <td>{{round($patient_prakriti->first_dominant_percentage, 2)}}%</td>                              </td>
+                               <td>{{$patient_prakriti->second_dominant_name}}</td>
+                               <td>{{round($patient_prakriti->second_dominant_percentage, 2)}}%</td>
+                               <td>{{$patient_prakriti->recessive_name}}</td>
+                               <td>{{round($patient_prakriti->recessive_percentage, 2)}}%</td>
+                               <td>{{($patient->blood_type)?$patient->blood_type->name:" "}}</td>
+                               <td>{{($patient->rh_factor)?$patient->rh_factor->code:" "}}</td>
                                 <?php   
-                                    if(isset($patient->fee)) {
-                                        $totalDays = floor((strtotime($patient->fee->end_date) - strtotime($patient->fee->start_date))/(60*60*24));
-                                       ?>
-                                      <td>
-                                   {{$totalDays}} 
-                                </td> 
-                                   <?php   } ?>
+                                  if(isset($patient->fee)) {
+                                     $totalDays = floor((strtotime($patient->fee->end_date) - strtotime($patient->fee->start_date))/(60*60*24));
+                                 ?>
+                               <td>{{$totalDays}}</td> 
+                                <?php   } ?>
                                         
-                                <td>
-                                 @if($patient->weight)
-                                 {{round($patient->weights->first()->weight,2)}}
-                                  @endif
-                                 </td>
-                                 <td>
-                                   @if($patient->weight)
-                                   {{round($patient->weights->last()->weight,2)}}
-                                   @endif
-                                   </td>
-                                
-                                <td>
-                                    @foreach ($patient->herbs as $herb)
-                                        {{$herb->herb->name}}, 
-                                    @endforeach
-                                </td>
-
-                              
-                            </tr>
+                               <td>{{($patient->weight)?round($patient->weights->first()->weight,2):" "}}</td>
+                               <td>{{($patient->weight)?round($patient->weights->last()->weight,2):" "}}</td>
+                               <td>@if(count($patient->herbs)) @foreach ($patient->herbs as $herb) {{$herb->herb->name}}, @endforeach @endif </td>
+                          </tr>
                         @endforeach
 
                         </tbody>
@@ -134,6 +84,7 @@ $(document).ready(function()
 {
     $('#table').dataTable({
       "iDisplayLength": 100,
+      "bPaginate": false,
       "aaSorting": [[ 1, "desc" ]]
     }); 
 
@@ -147,9 +98,11 @@ $(document).ready(function()
 
   $( "#downloadCSV" ).bind( "click", function() 
   {
+   
     var csv_value = $('#table').table2CSV({
                 delivery: 'value'
             });
+   
     downloadFile('patient_nutritionists.csv','data:text/csv;charset=UTF-8,' + encodeURIComponent(csv_value));
     $("#csv_text").val(csv_value);  
   });
