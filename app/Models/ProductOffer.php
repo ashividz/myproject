0732b4b1546202ba84dc5e4f967fb2d0e91082aa
= <=>
2016-03-19 13:40:43 +0530

    Fix bugs

--- app/Models/ProductOffer.php
+++ app/Models/ProductOffer.php
@@ -6,6 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductOffer extends Model
 {
+    protected $fillable = [
+        'product_id',
+        'minimum_quantity',
+        'product_offer_id',
+        'product_offer_quantity',
+        'start_date',
+        'end_date',
+        'created_by'
+    ];
+
     public function product()
     {
         return $this->hasOne(Product::class, 'id', 'product_offer_id');
