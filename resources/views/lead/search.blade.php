81b992eb0beff726897997b2a5c7305dee9f628a
sunil <sunil@drshikha.com>
2016-04-22 12:04:25 +0530

    access provided to goods sale department for lead search

--- resources/views/lead/search.blade.php
+++ resources/views/lead/search.blade.php
@@ -1,7 +1,7 @@
 <?php
     $readonly = "";
     $title = "";
-    if (Auth::user()->hasRole('cre')) {
+    if (Auth::user()->hasRole('cre') && !Auth::user()->hasRole('goods_sale')) {
         $readonly = "readonly";
         $title = "You do not have the permissions use this field. Please contact the Marketing Department.";
     }
