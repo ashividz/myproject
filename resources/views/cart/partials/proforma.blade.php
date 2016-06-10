25975d6f6d774e130b8f7ea81e7a68ed9fdf7c4f
saaz <cto@nutrihealthsystems.com>
2016-06-10 15:10:12 +0530

    Proforma PDF Format

--- resources/views/cart/partials/proforma.blade.php
+++ resources/views/cart/partials/proforma.blade.php
@@ -4,9 +4,15 @@
     <title>Proforma Invoice - {{ $cart->lead->name or "" }}</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 </head>
+<style type="text/css">
+    body {
+        font-size: 12px;
+        font-family: 'Open Sans';
+    }
+</style>
 <body>
     <div class="container">
-        <table class="table table-bordered" style="margin:20px">
+        <table class="tableq1" width="100%">
             <tr>
                 <td>
                     <div style="text-align:center">
@@ -26,7 +32,7 @@
                             <b>Proforma Invoice No :</b> {{ $cart->proforma->id or "" }}
                         </span>
                         <span class="pull-right">
-                            <b>Date :</b> {{ $cart->proforma->created_at->format('jS M, Y')}}
+                            <b>Date :</b> {{ $cart->proforma? $cart->proforma->created_at->format('jS M, Y') : "" }}
                         </span>
                     </div>
                     <div>
@@ -48,6 +54,9 @@
                         </div>
                     </div>
                 </td>
+            </tr>
+            <tr>
+                <hr>
             </tr>
             <tr>
                 <td>
@@ -107,17 +116,77 @@
                 </td>
             </tr>
             <tr>
-                <td>
+                <td style="font-size:11px">
                     <b>Terms & Conditions :</b>
-                    <span class="pull-right">Authorized Signatory</span>
                     <ul>
                         <li>The fee once received will not be refunded.</li>
                         <li>Cheque realization subject to Delhi jurisdiction.</li>
                         <li>Keeping in mind the complexity of the human body & behaviour it is not possible to gurantee weight loss.</li>
-                        <li>By making this payment you agree to receive phone calls, sms and emailers from Dr Shikha's Nutri-Health</li>
-                        <li>Cheque should be in favour of <b>Nutriwel Health (India) Pvt Ltd</b></li>
+                        <li>By making this payment you agree to receive phone calls, SMS and emailers from Dr Shikha's Nutri-Health</li>
+                        <li>Cheque should be in favour of <b>Nutriwel Health (India) Pvt. Ltd</b></li>
                     </ul>
                 </td>                    
+            </tr>
+            <tr>
+                <td>
+                    <hr style="border:1px dashed #111">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <table class="table1" width="100%">
+                        <tr>
+                            <td width="50%">
+                                <h3>PAYMENT ADVICE</h3>
+                                To:
+                                <div style="margin-left:20px">
+                                    Nutriwel Health (India) Pvt. Ltd<br>
+                                    232B, 2nd Floor<br>
+                                    Okhla Industrial Area<br>
+                                    Phase III<br>
+                                    New Delhi - 110020<br>
+                                </div>
+                            </td>
+                            <td>
+                                <table class="table1" width="100%">
+                                    <tr>
+                                        <td><label>Name: </label></td>
+                                        <td>{{ $cart->lead->name or "" }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td><label>Cart Id: </label></td>
+                                        <td>{{ $cart->id or "" }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td><label>Proforma Id: </label></td>
+                                        <td>{{ $cart->proforma->id or "" }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td><label>Proforma Date: </label></td>
+                                        <td>{{ $cart->proforma->created_at->format('jS M, Y') }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td><label>Amount: </label></td>
+                                        <td>{{ $cart->currency->name or "" }} {{ $cart->amount or "" }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="2">
+                                            <hr>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <label>Customer's Signature: </label>
+                                        </td>
+                                        <td>
+                                            --------------------------
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
             </tr>
         </table>
     </div>
