36af54eda44035d6e501d6b8216db38ae78ef158
purnenduDev <purnendu.chandan@gmail.com>
2016-07-14 15:04:25 +0530

    CRE leads & conversion report

--- app/Http/Controllers/SalesReportController.php
+++ app/Http/Controllers/SalesReportController.php
@@ -37,6 +37,16 @@ class SalesReportController extends Controller
         return view('home')->with($data);
     }
 
+     public function viewCreLeadStatus()
+    {
+        $data = array(
+            'menu'          =>  'sales',
+            'section'       =>  'reports.TL_wise_lead_conversion'
+        );
+
+        return view('home')->with($data);
+    }
+
     public function leadStatusReport(Request $request)
     {
         $start_date = $request->start_date ? $request->start_date : Carbon::now()->subDays(30); 
@@ -71,6 +81,73 @@ class SalesReportController extends Controller
                 }
             }
 
+            $cre->last = $last;
+            $cre->calls = $calls;
+            $cre->never = $never;
+            $counts = Status::select(DB::raw('m_lead_status.id, count(m.id) as cnt'))
+                        ->leftJoin('marketing_details as m', function($join) use($start_date, $end_date, $cre) {
+                            $join->on('m_lead_status.id', '=', 'm.status_id')
+                            ->where('created_at', '>=', $start_date)
+                            ->where('created_at', '<=', $end_date)
+                            ->where('cre_name', 'like', $cre->name);
+                        })
+                        ->groupBy('m_lead_status.id')                        
+                        ->get();
+
+            $cre->counts = $counts;
+
+            /*try {
+
+                $dialer_dispositions = DB::connection('pgsql')->table('ct_recording_log as crl')
+                            ->where('crl.phonenumber', '=', $lead->phone);
+            } catch (\Exception $e) {
+            
+                Session::flash("message", "Error connecting with Dialer Database");
+                Session::flash("status", "error");
+            }   */  
+        }
+
+        //dd($cres);
+        return $cres;
+    }
+
+        public function creConversionReport(Request $request)
+    {
+        $start_date = $request->start_date ? $request->start_date : Carbon::now()->subDays(30); 
+        $end_date = $this->end_date;
+        $conversion_last_date = $request->conversion_last_date;
+        //echo $start_date . " - ". $end_date."<br>";
+        $cres = User::getUsersByRole('cre', $request->user_id);
+        foreach ($cres as $cre) {
+            $leads = Lead::with('patient', 'patient.fee', 'dispositions')
+                    //->where('cre_name', $cre->name)
+                    ->whereBetween('created_at', array($start_date, $end_date))
+                    //->where('created_by', 'like', $cre->name)
+                    ->where('cre_name', 'like', $cre->name)
+                    ->get(); 
+
+            $cre->leads = $leads->count();
+
+            $last = 0;
+            $calls = 0;
+            $never = 0;
+            $converted_c = 0;
+            foreach ($leads as $lead) {
+              if($lead->patient && !is_null($lead->patient->fee) && $lead->patient->fee->cre==$cre->name && $lead->patient->fee->created_at < $conversion_last_date)
+                $converted_c++;
+            }
+             $cre->converted = $converted_c;
+             /* $converted = Lead::with('patient.fee')
+                                ->whereHas('patient.fee', function($q) use($conversion_last_date) {
+                                // Query the department_id field in status table
+                                 $q->where('created_at', '<', $conversion_last_date); // '=' is optional
+                                })
+
+                                //->where('cre_name', $cre->name)
+                                ->whereBetween('created_at', array($start_date, $end_date))
+                                ->where('created_by', $cre->name)
+                                ->get(); 
+            $cre->converted = $converted->count();*/
             $cre->last = $last;
             $cre->calls = $calls;
             $cre->never = $never;
