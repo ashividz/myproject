6b07cf0ada36ca33101984e29491718c56f6241b
sunil <sunil@drshikha.com>
2016-07-19 13:59:04 +0530

    added with trashed section in lead email

--- app/Models/Email.php
+++ app/Models/Email.php
@@ -8,7 +8,7 @@ class Email extends Model
 {
     public function user()
     {
-    	return $this->belongsTo(User::class);
+    	return $this->belongsTo(User::class)->withTrashed();
     }
 
     public function lead()
