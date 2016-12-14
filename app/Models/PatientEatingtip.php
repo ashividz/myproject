f54a5ff6b4311bc96e6c51f6b55a3958b19cdeed
jai <you@example.com>
2016-12-14 17:28:20 +0530

    changed the size of name field in patienteatingout table

--- app/Models/PatientEatingtip.php
+++ app/Models/PatientEatingtip.php
@@ -5,8 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-
-/* alter size of name fild */
+/* alter size of name field in  patient_eatingtips table */
 class PatientEatingtip extends Model
 {   
     protected $table = "patient_eatingtips";
