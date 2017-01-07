2f511dbeee46422ab4abaf5202cad4bbf1e7c48f
root <root@sunil.nutriwel.in>
2017-01-07 14:20:31 +0530

    completed back date weight update module

--- app/Http/Controllers/PatientWeightController.php
+++ app/Http/Controllers/PatientWeightController.php
@@ -301,4 +301,33 @@ class PatientWeightController extends Controller
         }
     }   
 
+    public function updateInitialWeight(Request $request, $id)
+    {
+        $patient = Patient::findOrFail($id);
+        $data = array(
+            'message'       =>  'Weight updated successfully',
+            'status'        =>  'success'
+        );
+
+        $this->validate($request, [
+            'initial_weight'      => 'required|numeric|min:1',
+            'initial_weight_date' => 'required|date',
+        ]);
+        $initialWeight = PatientWeight::where('patient_id',$patient->id)
+                    ->where('date',$request->initial_weight_date)
+                    ->first();
+        if ($initialWeight) {
+            $initialWeight->weight = $request->initial_weight;
+            $initialWeight->save();
+        } else {
+            PatientWeight::create([
+                'patient_id' => $patient->id,
+                'weight'     => $request->initial_weight,
+                'date'       => $request->initial_weight_date,
+                'created_by'  => Auth::id(),
+            ]);
+        }
+        return back()->with($data);
+    }
+
 }
