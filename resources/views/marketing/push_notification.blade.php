795ecdc662b2ab98e7327fbcb9fb6a784f7c4731
jairamsingh <jai.singh@drshikhasnutriwel.com>
2017-09-23 14:14:41 +0530

    yuwow notification

--- resources/views/marketing/push_notification.blade.php
+++ resources/views/marketing/push_notification.blade.php
@@ -3,8 +3,12 @@
 	<form method="post" action="/yuwow/yuwowNotification" style="margin-left: 20%">
 
 		<input type="hidden" name="_token" value="{{ csrf_token() }}">
+		<input type="radio" name="notification" value="NCR" checked>Delhi NCR
+  		<input type="radio" name="notification" value="PAN"> Pan India
+  		<input type="radio" name="notification" value="INT"> International<br>
 		<h4>Title</h4> <input type="text" name="title" placeholder="Enter title" required>
 		<br>
+		
 		<h4>Notification</h4><textarea rows="6" cols="90" name="message" placeholder="Enter message" required></textarea>
 		<br>
 		<button type="submit" class="btn btn-primary">Send </button>
