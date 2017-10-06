2f09148fc1e8b8b67ce78e096d7aa6b4e68bfeaf
Saaz Rai <cto@nutrihealthsystems.com>
2017-10-06 12:27:38 +0530

    rto email

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -16,106 +16,120 @@ use Auth;
 class EmailController extends Controller
 {
 
-	public function show($id)
-	{
-		$lead = Lead::find($id);
-		$emails = Email::where('lead_id', $id)
-					->orderBy('created_at', 'DESC')
-					->limit('20')
-					->get();
+    public function show($id)
+    {
+        $lead = Lead::find($id);
+        $emails = Email::where('lead_id', $id)
+                    ->orderBy('created_at', 'DESC')
+                    ->limit('20')
+                    ->get();
 
-		$templates = EmailTemplate::where('email_template_category_id',1)
-					->with('attachments')->get();
+        $templates = EmailTemplate::where('email_template_category_id',1)
+                    ->with('attachments')->get();
 
-		$data = array(
-            'menu'          => 'lead',
-            'section'       => 'partials.email',
-            'lead'			=>	$lead,
-            'templates'		=>	$templates,
-            'emails'		=>	$emails,
-            'i'				=> 	'1'
-        );
+        $data = array(
+'menu' => 'lead',
+'section' => 'partials.email',
+'lead'          =>  $lead,
+'templates'     =>  $templates,
+'emails'        =>  $emails,
+'i'             =>  '1'
+);
 
-        return view('home')->with($data);
-	}
+return view('home')->with($data);
+    }
 
-	public function send(Request $request, $id)
-	{
-		//dd($request);
-		$sms = NULL;
-		$lead = Lead::find($id);
-		$template = EmailTemplate::find($request->template_id);
-		$status = '';
+    public function send(Request $request, $id)
+    {
+        
+        $sms = NULL;
+        $lead = Lead::find($id);
+        $template = EmailTemplate::find($request->template_id);
+        $rtodetails = $request->rtodetails;
+        $status = '';
 
-		if ($lead->country == "IN" && $template->sms <> "") {
-			$sms = $this->sendSMS($lead, $template);
-			$status .= 'SMS Sent';
-		}
+        if ($lead->country == "IN" && $template->sms <> "") {
+            $sms = $this->sendSMS($lead, $template);
+            $status .= 'SMS Sent';
+        }
+        
+        if (trim($lead->email) == '') {
+            return redirect('lead/'.$id.'/email')->with('status', $status); # code...
+        }       
 
-		if (trim($lead->email) == '') {
-			return redirect('lead/'.$id.'/email')->with('status', $status);	# code...
-		}		
+        if($template->email <> "")
+        {
+            $this->sendEmail($lead, $template,$rtodetails,$sms);
+            $status .= 'Email Sent';
+        }
+        
+        return redirect('lead/'.$id.'/email')->with('status', $status);
+    }
 
-		if($template->email <> "")
-		{
-			$this->sendEmail($lead, $template);
-		}
 
-		//Save
-		$email = new Email();
-		$email->user_id = Auth::user()->id;
-		$email->lead_id = $lead->id;
-		$email->template_id = $request->template_id;
-		$email->email = str_replace('$client', $lead->name, $template->email);
-		$email->sms_response = $sms;
+    private function sendSMS($lead, $template)
+    {
+        $sms = new SMS();
+        return $sms->send($lead->mobile, $template->sms);
+    }
 
-		$email->save();
+    private function sendEmail($lead, $template,$rtostring,$sms)
+    {
+        //dd($rtostring);
+        $body = $template->email;
+        if($template->id==77)
+        {
+            $rtodetails = explode(",",$rtostring);
+            $body = str_replace('$invoice', $rtodetails[0], $body);
+            $body = str_replace('$tracking', $rtodetails[1], $body);
+            $body = str_replace('$courier', $rtodetails[2], $body);
+            $body = str_replace('$payment', $rtodetails[3], $body);
+            $body = str_replace('$product', $rtodetails[4], $body);
+            $body = str_replace('$amount', $rtodetails[5], $body);
+            $body = str_replace('$reason', $rtodetails[6], $body);
+        }
+                
+        $body = str_replace('$customer', $lead->name, $body);
+        $body = str_replace('$client', $lead->name, $body);
+        $body = str_replace('$cre', Auth::user()->employee->name, $body);
+        
+        if (Auth::user()->hasRole('nutritionist')) {
+            $body = str_replace('$nutritionist', 'Nutritionist ' . Auth::user()->employee->name, $body);
+        }
+        else
+        {
+            $body = str_replace('$nutritionist', 'Nutritionist', $body);
+        }
+        
+        Mail::send([], [], function($message) use ($body, $lead, $template)
+        {
+         $from = isset($template->from) ? $template->from : 'sales@nutrihealthsystems.com';
+        
+         $message->to($lead->email, $lead->name)
+         ->subject($template->subject)
+         ->from($from, 'Nutri-Health Systems' );
+        
+         //Add CC
+         if (trim($lead->email_alt) <> '') {
+                $message->cc($lead->email_alt, $name = null);
+            }
 
-		$status .= 'Email Sent';
-		
-		return redirect('lead/'.$id.'/email')->with('status', $status);
-	}
+         $message->setBody($body, 'text/html');
 
-	private function sendSMS($lead, $template) 
-	{
-		$sms = new SMS();
-		return $sms->send($lead->mobile, $template->sms);
-	}
+            //Add attachments
+            foreach ($template->attachments as $attachment) {
+                $message->attachData($attachment->file, $attachment->name);
+            }
+        });
 
-	private function sendEmail($lead, $template) 
-	{
-		$body = $template->email;
-		$body = str_replace('$customer', $lead->name, $body);
-		$body = str_replace('$client', $lead->name, $body);
-		$body = str_replace('$cre', Auth::user()->employee->name, $body);
+        //save email
+        $email = new Email();
+        $email->user_id = Auth::user()->id;
+        $email->lead_id = $lead->id;
+        $email->template_id = $template->id;
+        $email->email = $body;
+        $email->sms_response = $sms;
 
-		if (Auth::user()->hasRole('nutritionist')) {
-			$body = str_replace('$nutritionist', 'Nutritionist ' . Auth::user()->employee->name, $body);
-		}
-		else
-		{
-			$body = str_replace('$nutritionist', 'Nutritionist', $body);
-		}
-		
-		Mail::send([], [], function($message) use ($body, $lead, $template)
-		{
-		    $from = isset($template->from) ? $template->from : 'sales@nutrihealthsystems.com';
-		    
-		    $message->to($lead->email, $lead->name)
-		    ->subject($template->subject)
-		    ->from($from, 'Nutri-Health Systems' );
-		    
-		    //Add CC
-		    if (trim($lead->email_alt) <> '') {
-		    	$message->cc($lead->email_alt, $name = null);
-		    }
-
-		    $message->setBody($body, 'text/html');
-
-			//Add attachments
-			foreach ($template->attachments as $attachment) {
-				$message->attachData($attachment->file, $attachment->name);
-			}
-		});
-	}
+        $email->save();
+    }
 }
