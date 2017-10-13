a852c313d4f66e7524ae623ce6d94a7e88c82daf
Akriti <akriti.kumari@drshikhasnutriwel.com>
2017-10-13 14:26:50 +0530

    Yuwow gluco CRM

---
+++ app/Models/YuWoW/GlucoOne.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\YuWoW;
+
+use Illuminate\Database\Eloquent\Model;
+
+use App\Models\YuWoW\User;
+
+class GlucoOne extends Model
+{
+    protected $table = 'yuwow_alpha_1_0.gluco_one';
+}
