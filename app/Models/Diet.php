6fc2a9315faa0cd6f9d4c508bec6601d51b891ac
jairamsingh <jai.singh@drshikhasnutriwel.com>
2018-08-08 09:53:45 +0530

    diet change

--- app/Models/Diet.php
+++ app/Models/Diet.php
@@ -136,13 +136,13 @@ class Diet extends Model
             $status  = 'success'; 
             Diet::setAppResponse($diets,$app_response);
 
-            $client = new Client();
-            $app_response = $client->request('POST', 'https://myapplication-47c35.appspot.com/api/user/storeDiets', [
-                    'form_params' => [
-                    'api_token'=>$code,
-                    'diet' => json_encode($diets),
-                    'email' => json_encode($email)]
-                    ]);
+            // $client = new Client();
+            // $app_response = $client->request('POST', 'https://myapplication-47c35.appspot.com/api/user/storeDiets', [
+            //         'form_params' => [
+            //         'api_token'=>$code,
+            //         'diet' => json_encode($diets),
+            //         'email' => json_encode($email)]
+            //         ]);
                   
         }
 
