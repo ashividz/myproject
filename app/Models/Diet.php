c27abd21f5adf67f4ffd9be5c5ae2c993a07580a
jairamsingh <jai.singh@drshikhasnutriwel.com>
2019-01-12 12:17:18 +0530

    diet changes

--- app/Models/Diet.php
+++ app/Models/Diet.php
@@ -129,7 +129,7 @@ class Diet extends Model
 
             Diet::AddVediqueDiet($diets , $email);
             $client = new Client();
-            $app_response = $client->request('POST', 'https://portal.yuwow.com/index.php/diet/insertDiet', [
+            $app_response = $client->request('POST', 'https://portal.yuwow.com/index.php/diet/insertDiet', ['verify' => false] ,[
                     'form_params' => [
                     'diet' => json_encode($diets),
                     'email' => json_encode($email)]
