0602608f1e5b62ba47852d5aff52f41c7de59db6
jairamsingh <jai.singh@drshikhasnutriwel.com>
2019-01-22 12:41:44 +0530

    hebs edit

--- resources/views/doctor/partials/daterange_users.blade.php
+++ resources/views/doctor/partials/daterange_users.blade.php
@@ -1,4 +1,4 @@
-@if(Auth::user()->hasRole('admin') || Auth::user()->hasRole('service')|| Auth::user()->hasRole('service_tl'))
+@if(Auth::user()->hasRole('admin') || Auth::user()->hasRole('service')|| Auth::user()->hasRole('service_tl') || Auth::user()->hasRole('senior_doctor'))
 	@include('../partials/daterange_users')
 @else
 	@include('../partials/daterange')
